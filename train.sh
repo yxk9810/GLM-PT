d36cd0aefe3f2d62a2d6dd564f3b36f5b05e8e04
JackieWu <wujindou1@163.com>
2023-04-04 12:46:23 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -8,11 +8,11 @@ CUDA_VISIBLE_DEVICES=0 python3 main.py \
     --prompt_column content \
     --response_column summary \
     --overwrite_cache \
-    --model_name_or_path silver/chatglm-6b-slim \
+    --model_name_or_path THUDM/chatglm-6b-int4 \
     --output_dir output/adgen-chatglm-6b-pt-$PRE_SEQ_LEN-$LR \
     --overwrite_output_dir \
-    --max_source_length 64 \
-    --max_target_length 64 \
+    --max_source_length 384 \
+    --max_target_length 128 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 16 \
