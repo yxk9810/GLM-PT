fba39ec8d7d1e3d252096934f14372ab51def3b1
JackieWu <wujindou1@163.com>
2023-04-07 16:25:58 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -13,14 +13,14 @@ CUDA_VISIBLE_DEVICES=0 python3 main.py \
     --overwrite_output_dir \
     --max_source_length 384 \
     --max_target_length 128 \
-    --per_device_train_batch_size 1 \
+    --per_device_train_batch_size 2 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 16 \
     --predict_with_generate \
-    --max_steps 3000 \
+    --max_steps 1000 \
     --logging_steps 10 \
     --save_steps 1000 \
     --learning_rate $LR \
     --pre_seq_len $PRE_SEQ_LEN \
-#     --quantization_bit 4
+    --quantization_bit 4
 
